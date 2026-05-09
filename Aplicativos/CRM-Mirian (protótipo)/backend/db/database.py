edd09acfadf892d35c17bca96cc2ab8431211eba
Sallesg82 <davisallesg@yahoo.com>
2026-05-08 23:37:51 -0300

    Criação de um banco de dados a parte que conecta com todos os aplicativos.

--- Aplicativos/CRM-Mirian (protótipo)/backend/db/database.py
+++ Aplicativos/CRM-Mirian (protótipo)/backend/db/database.py
@@ -2,7 +2,7 @@ import sqlite3
 import os
 from datetime import datetime
 
-DB_PATH = os.path.join(os.path.dirname(__file__), 'beautyflow.db')
+DB_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..', 'DB', 'beautyflow.db'))
 
 
 def get_db():
